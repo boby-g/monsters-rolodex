d19dc5c344b13e41727fe5c145e324feba9650de
geo1976 <bobescugeorge@yahoo.com>
2020-05-03 17:57:56 +0100

    Updates

---
+++ precache-manifest.fc5aa9040f31efba45dfadd0a76c5210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f1f5f5bccd4b0bd09cc66e825cb600",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cc5871876c004743349a",
+    "url": "/monsters-rolodex/static/css/main.bd6513f7.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5871876c004743349a",
+    "url": "/monsters-rolodex/static/js/main.6b8697ef.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
